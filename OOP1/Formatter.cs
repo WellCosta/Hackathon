907572df553aaaf7f842dcb8d75e83b88b671a40
Wellington Gesteira <wellington-47@hotmail.com>
2018-11-16 10:54:44 +0100

    Initial commit

---
+++ OOP1/Formatter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace OOP1
+{
+    interface IFormatter
+    {
+        string Format(string text);
+    }
+
+    /// <summary>
+    /// Formats a text to pascal case
+    /// </summary>
+    class Formatter : IFormatter
+    {
+        // Begin: Cannot change 
+        public string Format(string text)
+        {
+            return ToPascalCase(text);
+        }
+
+        private string ToPascalCase(string text)
+        {
+            return text.Substring(0, 1).ToUpper() + text.Substring(1);
+        }
+        // End: Cannot change
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string text = "hello world!";
+            var formatter = new Formatter();
+            var result = formatter.Format(text);
+            if (result != "Hello world!")
+            {
+                Console.WriteLine("Wrong!");
+            }
+
+            var secondResult = formatter.Format(text);
+            if (secondResult != "Hello World!")
+            {
+                Console.WriteLine("Wrong!");
+            }
+        }
+    }
+}
